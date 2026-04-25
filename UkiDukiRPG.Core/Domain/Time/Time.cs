43d2d5c6fd8c068d2a4bf4432f5dce56e779c6da
Nemanja <nradovanovic12022rn@raf.rs>
2026-04-25 15:35:34 +0200

    Core | Implement Very Simple Time System

---
+++ UkiDukiRPG.Core/Domain/Time/Time.cs
@@ -0,0 +1,30 @@
+﻿namespace UkiDukiRPG.Core.Domain.Time;
+
+public enum TimeUnit
+{
+    Tick,
+    Turn,
+    Round,
+}
+
+public readonly struct TimeInterval(int ticks = 0)
+{
+    public int Ticks { get; } = ticks;
+
+    public static TimeInterval FromTurns(int turns) => new(turns);
+
+    public static TimeInterval FromRounds(int rounds) => new(2 * rounds);
+
+    public static TimeInterval FromTicks(int ticks) => new(ticks);
+
+    public static TimeInterval From(int interval, TimeUnit timeUnit)
+    {
+        return timeUnit switch
+               {
+                   TimeUnit.Tick  => FromTicks(interval),
+                   TimeUnit.Turn  => FromTurns(interval),
+                   TimeUnit.Round => FromRounds(interval),
+                   _              => FromTicks(0)
+               };
+    }
+}
---
+++ UkiDukiRPG.Core/Domain/Time/TimeSystem.cs
@@ -0,0 +1,30 @@
+﻿namespace UkiDukiRPG.Core.Domain.Time;
+
+public class TimeSystem
+{
+    private readonly PriorityQueue<Action, int> m_Queue = new();
+
+    private int m_CurrentTick = 0;
+
+    public void AdvanceTick()
+    {
+        ++m_CurrentTick;
+
+        while (m_Queue.TryPeek(out var action, out var executionTick))
+        {
+            if (executionTick > m_CurrentTick)
+                break;
+
+            m_Queue.Dequeue();
+
+            action();
+        }
+    }
+
+    public void Schedule(Action action, TimeInterval interval) => m_Queue.Enqueue(action, m_CurrentTick + interval.Ticks);
+
+    // @formatter:off
+    public void Schedule(Action action, int interval, TimeUnit delayUnit) => m_Queue.Enqueue(action, 
+                                                                                          m_CurrentTick + TimeInterval.From(interval, delayUnit).Ticks);
+    // @formatter:on
+}
